84719dffae0ecb843606e2f01563244b365112c2
=LC Thanh <lcthanh.htvn@gmail.com>
2025-04-21 17:43:33 +0700

    update: logout page

--- fime_web_app/src/app/(auth)/logout/page.tsx
+++ fime_web_app/src/app/(auth)/logout/page.tsx
@@ -1,22 +1,12 @@
 "use client";
 
-import authApiRequests from "@/requests/auth.request";
-import { signOut as signOutClient } from "next-auth/react";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { LogoutChild } from "@/app/(auth)/logout/logout-child";
+import { Suspense } from "react";
 
 export default function LogoutPage() {
-  const searchParams = useSearchParams();
-  const refresh_token = searchParams.get("token");
-
-  useState(async () => {
-    if (!refresh_token) return;
-    await authApiRequests.logout(refresh_token || "");
-  });
-
-  useEffect(() => {
-    signOutClient({ redirectTo: "/login" });
-  }, []);
-
-  return <p>Redirecting to login...</p>;
+  return (
+    <Suspense>
+      <LogoutChild />
+    </Suspense>
+  );
 }
